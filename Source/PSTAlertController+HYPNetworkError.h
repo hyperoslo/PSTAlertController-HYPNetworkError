4f82f7df32ff64578c370c996dfc6f573fcc6e5a
Elvis Nuñez <NSElvis@users.noreply.github.com>
2014-12-01 16:08:57 +0100

    Switch API positions

--- Source/PSTAlertController+HYPNetworkError.h
+++ Source/PSTAlertController+HYPNetworkError.h
@@ -2,8 +2,8 @@
 
 @interface PSTAlertController (HYPNetworkError)
 
-+ (instancetype)presentNetworkErrorAlertWithTitle:(NSString *)title error:(NSError *)error controller:(UIViewController *)controller;
-
 + (instancetype)presentNetworkErrorAlertWithError:(NSError *)error controller:(UIViewController *)controller;
+
++ (instancetype)presentNetworkErrorAlertWithTitle:(NSString *)title error:(NSError *)error controller:(UIViewController *)controller;
 
 @end
