ce91c87f22ca7fd1c86e1d67705525506652db03
Elvis Nuñez <elvisnunez@me.com>
2014-12-01 16:07:36 +0100

    Add method to supply title too

--- Source/PSTAlertController+HYPNetworkError.m
+++ Source/PSTAlertController+HYPNetworkError.m
@@ -4,9 +4,14 @@
 
 + (instancetype)presentNetworkErrorAlertWithError:(NSError *)error controller:(UIViewController *)controller
 {
+    [self presentNetworkErrorAlertWithTitle:NSLocalizedString(@"NetworkErrorTitle", nil) error:error controller:controller];
+}
+
++ (instancetype)presentNetworkErrorAlertWithTitle:(NSString *)title error:(NSError *)error controller:(UIViewController *)controller
+{
     NSString *message = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"NetworkErrorMessage", nil), [error localizedDescription]];
 
-    PSTAlertController *alertController = [self alertWithTitle:NSLocalizedString(@"NetworkErrorTitle", nil) message:message];
+    PSTAlertController *alertController = [self alertWithTitle:title message:message];
     [alertController addAction:[PSTAlertAction actionWithTitle:NSLocalizedString(@"Dismiss", @"") style:PSTAlertActionStyleCancel handler:nil]];
     [alertController showWithSender:nil controller:controller animated:YES completion:nil];
 
